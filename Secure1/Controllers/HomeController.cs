b5e5d13bccbf813f5eeb0e895054f832dfd5531f
Mike K <mike.keena@thekeenas.com>
2017-11-21 15:49:26 -0600

    With Basic now page/controller/view added (Upload)

--- Secure1/Controllers/HomeController.cs
+++ Secure1/Controllers/HomeController.cs
@@ -21,37 +21,7 @@ namespace Secure1.Controllers
 		public IActionResult About() {
 			ViewData["Message"] = "Your application description page.";
 
-			//Services.EmailSender.SendEmailBlocking("michael.keena@geha.com", "Test Email","This is a test email!");
-
-			//string toEmail = "michael.keena@geha.com";
-			//string message = "Test Message - code in about.";
-			//string subject = "This is the Test Subject";
-			//MailMessage mail = new MailMessage() {
-			//	From = new MailAddress("mike.keena@thekeenas.com", "Mike Keena")
-			//};
-			//mail.To.Add(new MailAddress(toEmail));
-			////mail.CC.Add(new MailAddress(_emailSettings.CcEmail));
-			//mail.Sender = new MailAddress("michael.keena@geha.com");
-			//mail.Subject = "Blocking Test - " + subject;
-			//mail.Body = message;
-			//mail.IsBodyHtml = true;
-			//mail.Priority = MailPriority.High;
-			//mail.BodyEncoding = Encoding.UTF8;
-			//mail.SubjectEncoding = Encoding.UTF8;
-			//using (SmtpClient smtp = new SmtpClient("smtpout.secureserver.net", 465)) {
-			//	smtp.EnableSsl = true;
-			//	smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-			//	smtp.UseDefaultCredentials = false;
-			//	smtp.Credentials = new NetworkCredential("mike.keena@thekeenas.com", "astrid");
-			//	//smtp.Send(mail);
-			//	try {
-			//		smtp.Send(mail);
-			//	} catch (Exception e) {
-			//		throw e;
-			//	}
-
 			return View();
-			//}
 		}
 
         public IActionResult Contact()
---
+++ Secure1/SQL/Learn1Log.sql
@@ -0,0 +1,25 @@
+﻿USE [IA-DB-1]
+GO
+
+SELECT	* FROM dbo.[Log]
+SELECT * FROM dbo.AspNetUsers
+
+INSERT INTO dbo.Log
+(
+    Message,
+    MessageTemplate,
+    Level,
+    TimeStamp,
+    Exception,
+    Properties,
+    LogEvent
+)
+VALUES
+(   N'Manual',                 -- Message - nvarchar(max)
+    N'Manual',                 -- MessageTemplate - nvarchar(max)
+    N'1',                 -- Level - nvarchar(128)
+    SYSDATETIMEOFFSET(), -- TimeStamp - datetimeoffset(7)
+    N'exc',                 -- Exception - nvarchar(max)
+    NULL,                -- Properties - xml
+    N'event'                  -- LogEvent - nvarchar(max)
+)
